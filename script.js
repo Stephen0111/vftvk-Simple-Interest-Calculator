46e2f6bb4e2110969f5b7417b3018180fec3e2ea
stephen adegbile <stephen.adegbile@icloud.com>
2021-09-13 13:46:17 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-function compute()
-{
-     var year = new Date().getFullYear()+parseInt(years);
-var principal = document.getElementById("principal").value;
- var rate = document.getElementById("rate").value;
-     var years = document.getElementById("years").value;
-      var interest = principal * years * rate /100;
-          document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-}
-      function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-}  
-function valid()
-{
-    if (principal.value == "")
-{ alert("Please enter a valid amount");
-            principal.focus();
-            return false;}
+function compute() {
+    //Retrieve the values 
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + years;
+    //Create the Interest text
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>";
 
- if (principal.value <0)
-{
-    alert("Please enter a positive number");
-    principal.focus();
-    return false;
 }
-return true;
+
+//update ther rate value
+function getSliderValue() {
+    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
+}
+
+//Check for positive values
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
 }
